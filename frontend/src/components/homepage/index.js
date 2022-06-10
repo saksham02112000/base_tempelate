e92a23432bc765e46fe1dd7a448062529480b5dc
Saksham <sakshamsrini@gmail.com>
2022-06-10 19:46:32 +0530

    da

--- frontend/src/components/homepage/index.js
+++ frontend/src/components/homepage/index.js
@@ -3,11 +3,16 @@ import {AuthContext} from "../../context/authcontext";
 import {useContext} from "react";
 
 export default function Homepage() {
+
+
     const {logout} = useContext(AuthContext);
     return (
         <div>
             Homepage
-            <button onClick={logout}> Logout </button>
+            jnaspdasdn
+            <br />
+            <h1>hdhdasio</h1>
+            <button className="button" onClick={logout}> Logout </button>
         </div>
     )
 }
